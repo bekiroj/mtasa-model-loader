3301c16185ad6d6f02ea57088b89b62cf1837bda
Bekir Aşkar <bekiraskar7@icloud.com>
2022-09-14 16:27:07 +0300

    collector.exe

---
+++ collector/collector/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace collector
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.collect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // collect
+            // 
+            this.collect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.collect.Location = new System.Drawing.Point(308, 12);
+            this.collect.Name = "collect";
+            this.collect.Size = new System.Drawing.Size(75, 23);
+            this.collect.TabIndex = 0;
+            this.collect.Text = "browse";
+            this.collect.UseVisualStyleBackColor = true;
+            this.collect.Click += new System.EventHandler(this.collect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 53);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.collect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "model collector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button collect;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ collector/collector/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+
+namespace collector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void collect_Click(object sender, EventArgs e)
+        {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+                dlg.ShowDialog();
+                textBox1.Text = dlg.SelectedPath;
+
+                DirectoryInfo dir_info = new DirectoryInfo(dlg.SelectedPath);
+                string directory = dir_info.Name;
+
+                string filePath = @dlg.SelectedPath + "meta.txt";
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                var files = System.IO.Directory.GetFiles(dlg.SelectedPath, "*.*", System.IO.SearchOption.AllDirectories);
+
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    foreach (string index in files)
+                    {
+                        string fileName = @index;
+                        string fileModel = @index;
+                        string result;
+                        result = Path.GetFileName(fileName);
+                        int fileExtPos = result.LastIndexOf(".");
+                        if (fileExtPos >= 0)
+                            fileModel = result.Substring(0, fileExtPos);
+
+                        sw.WriteLine("<file src='" + directory + "/" + result + "' download='false' model='" + fileModel + "'/>");
+                    }
+                }
+
+                MessageBox.Show("models collected to 'meta.txt'", "success");
+        }
+    }
+}
